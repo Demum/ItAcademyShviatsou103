e5f39d05a4d64f7793dec9f2e183eae1bd2539ad
Yauhen Shviatsou <demum1930@gmail.com>
2022-07-08 21:45:48 +0300

    Fix3 OnlinerTest

--- src/test/java/OnlinerTest.java
+++ src/test/java/OnlinerTest.java
@@ -2,26 +2,38 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 
+import java.sql.DriverManager;
+import java.util.concurrent.TimeUnit;
+
 public class OnlinerTest extends WebDriverSettings {
+    By samsungLocator = By.xpath("//span[contains(text(), 'Телевизор Samsung')]");
 
     @Test
-    public void onlinerSiteTest () throws InterruptedException {
+    public void onlinerSiteTest() throws InterruptedException {
         driver.get("https://www.onliner.by/");
-                  driver.findElement(By.xpath("//a[contains(text(), 'Каталог')][1]")).click();
+        driver.findElement(By.xpath("//a[contains(text(), 'Каталог')][1]")).click();
 
-                 driver.findElement(By.xpath("//span[contains(text(), 'Электроника')] ")).click();
-
+        driver.findElement(By.xpath("//span[contains(text(), 'Электроника')] ")).click();
+        Thread.sleep(1000);
         driver.findElement(By.xpath("//*[text()='Телевизоры']")).click();
 
-        JavascriptExecutor jse = (JavascriptExecutor)driver;
+        JavascriptExecutor jse = (JavascriptExecutor) driver;
         jse.executeScript("window.scrollBy(0,250)", "");
-        Thread.sleep(1000);
-               driver.findElement(By.xpath("//span[contains(text(), 'LG')] ")).click();
-Thread.sleep(1000);
 
- driver.findElement(By.xpath("//span[contains(text(), 'Телевизор Samsung')]" ));
+        driver.findElement(By.xpath("//span[contains(text(), 'LG')] ")).click();
 
-        }
+        Assert.assertFalse(samsungNotDisplayed());
     }
+
+    public boolean samsungNotDisplayed() {
+        try {
+            driver.findElement(samsungLocator).isDisplayed();
+        } catch (NoSuchElementException e) {
+        }
+        return false;
+
+    }
+}
